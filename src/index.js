dc76c1bdb4982360fdc7cbdb36c683a2f60c30df
mohamedterk02 <mohamedtarek.m29@gmail.com>
2025-10-27 21:21:27 +0300

    remove react query

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import "./index.css";
 
 import "./index.css";
 import App from "./App";
 import store from "./store";
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </Provider>
 );
